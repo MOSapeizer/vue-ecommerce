764fb20c9bb3d9bd551b571439548ca6b937f9c4
mosapeizer <k471352@gmail.com>
2018-01-08 21:14:46 +0800

    新增一種方案的產品頁

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="container p-0">
     <div class="information" v-html="information"></div>
+    <div class="add-to-cart-panel">
+      <div class="title" v-text="title"></div>
+      <select name="product" id="" v-model="selectedProductId">
+        <option v-for="(product, key) in products" :value="product.id" :key="product.id" v-text="product.title"></option>
+      </select>
+      <span class="price">{{ selectedProductPrice }}</span>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'landing-page',
+    created () {
+      this.selectedProductId = this.hasProducts() ? this.products[0].id : undefined
+    },
+    methods: {
+      hasProducts () {
+        return this.products.length > 0
+      }
+    },
+    computed: {
+      selectedProductPrice () {
+        let product = this.products.find(product => product.id === this.selectedProductId)
+        return product ? product.price : 0
+      }
+    },
     data () {
       return {
-        information: '<img width="100%" src="https://dreamkisstw.com/images/1512627085372.jpg" alt="">'
+        title: '',
+        information: '',
+        products: [],
+        selectedProductId: undefined
       }
     }
   }
--- test/unit/LandingPage.spec.js
+++ test/unit/LandingPage.spec.js
@@ -3,19 +3,78 @@ import LandingPage from '~/components/LandingPage'
 import { helper } from '~/test/helper'
 
 describe('LandingPage.vue', () => {
-  let wrapper, expectTo
+  let wrapper, expectTo, model
 
-  beforeEach(() => {
-    wrapper = mount(LandingPage)
-    expectTo = helper(wrapper, expect)
+  describe('產品資訊區塊', () => {
+
+    beforeEach(() => {
+
+      model = {
+        information: '<img id="testImage" src="sample.jpg" alt="">',
+      }
+
+      wrapper = mount(LandingPage, {data: model})
+      expectTo = helper(wrapper, expect)
+    })
+
+    it('要有產品資訊', () => {
+      expectTo.contain('.information')
+      expectTo.see(model.information, '.information')
+    })
   })
 
-  it('要有產品資訊', () => {
-    expectTo.contain(".information")
+  describe('產品選擇區塊', () => {
 
-    const information = '<img id="testImage" src="sample.jpg" alt="">'
-    wrapper.setData({ information })
+    const cartPanel = '.add-to-cart-panel'
 
-    expectTo.see(information, '.information')
+    beforeEach(() => {
+      model = {
+        title: '產品模組標題',
+        products: [
+          {id: 1, title: '產品方案', price: 350},
+          {id: 2, title: '產品方案2', price: 450},
+        ]
+      }
+
+      wrapper = mount(LandingPage, {data: model})
+      expectTo = helper(wrapper, expect)
+    })
+
+    it('要存在', () => {
+      expectTo.contain(`${cartPanel}`)
+    })
+
+    it('顯示產品標題', () => {
+      const $productTitle = `${cartPanel} .title`
+      expectTo.contain($productTitle)
+      expectTo.see(model.title, $productTitle)
+    })
+
+    it('預設顯示第一個產品的資訊', () => {
+      const $productSelect = `${cartPanel} select`
+      expectTo.contain($productSelect)
+      expectTo.see('產品方案', $productSelect)
+
+      const $productPrice = `${cartPanel} .price`
+      expectTo.contain($productPrice)
+      expectTo.see(model.products[0].price, $productPrice)
+    })
+
+    it('選擇產品後，要顯示對應的價錢', () => {
+      // Vue-test-utils 並沒有提供更好的方法：
+      // 要追蹤這個 issue 請到 https://github.com/vuejs/vue-test-utils/issues/260
+      wrapper.setData({
+        selectedProductId: 2
+      })
+
+      // 我比較想用的方法
+      // const $productOptions = `${cartPanel} select option`
+      // wrapper.findAll($productOptions).at(1).trigger('click')
+
+      const $productPrice = `${cartPanel} .price`
+      expectTo.contain($productPrice)
+      expectTo.see(model.products[1].price, $productPrice)
+    })
   })
+
 })
